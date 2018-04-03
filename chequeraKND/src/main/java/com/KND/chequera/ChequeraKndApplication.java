abac53543d3e598f0f98724289dbfd66441365b9
MrGonzalezCruz <mrgonzalezcruz@gmail.com>
2018-04-03 09:53:18 -0500

    Primer commit del proyecto, cargo modelo de la Base de Datos y la estructura del proyecto

---
+++ chequeraKND/src/main/java/com/KND/chequera/ChequeraKndApplication.java
@@ -0,0 +1,12 @@
+package com.KND.chequera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChequeraKndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChequeraKndApplication.class, args);
+	}
+}
